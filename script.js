c8bf9496717e214ae0e17160d9fbecaa037ab89d
Nebular6 <104096469+Nebular6@users.noreply.github.com>
2024-02-02 15:46:29 +0000

    added interactivity

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function loaded() {
     console.log("The page has now loaded");
 }
+function display(x){
+    console.log("hello")
+    var text =" "
+    var id = " "
+    switch(x){
+        case 1:
+            console.log("hello 2")
+            text="po would not be prosocuted";
+            id = "a";
+            break;
+        case 2:
+            text ="tinky winky would be a terrorist";
+            id = "b";
+            break;
+        case 3:
+            text ="laa-laa would be a terrorist";
+            id = "c";
+            break;
+        case 4:
+            text ="dispy would be a terrorist";
+            id = "d";
+            break;
+    }
+    document.getElementById(id).innerHTML= text ;
+}
